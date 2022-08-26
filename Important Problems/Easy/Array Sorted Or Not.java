83ffc705f73dfeb83f6a5af51cee7b76bd841fca
Sanket Jagdale <93209316+Sanket00900@users.noreply.github.com>
2022-08-27 01:22:03 +0530

    Create Array Sorted Or Not.java

---
+++ Important Problems/Easy/Array Sorted Or Not.java
@@ -0,0 +1,20 @@
+
+public class ArraySortedOrNotRecursion {
+    public static boolean isSorted(int[]arr,int startIndex){
+        if(startIndex == arr.length - 1){
+            return true;
+        }
+        if(arr[startIndex] > arr[startIndex+1]){
+            return false;
+        }
+
+        boolean ans = isSorted(arr, startIndex+1);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[]arr = {2,3,5,6,7,9};
+        System.out.println(isSorted(arr,0));
+    }
+
+}
